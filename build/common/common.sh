222d69c797f1a7a5485ddc689e369cf16874bfb8
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-18 20:38:03 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -834,6 +834,7 @@ svn co https://github.com/kiddin9/luci-app-wizard/trunk package/waynesg/luci-app
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/waynesg/luci-app-advanced
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wrtbwmon package/waynesg/luci-app-wrtbwmon
 svn co https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/waynesg/luci-app-wrtbwmon/wrtbwmon
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/waynesg/luci-app-socat
 else
 cat >>"${HOME_PATH}/feeds.conf.default" <<-EOF
 src-git waynesg https://github.com/waynesg/OpenWrt-Software.git;js
