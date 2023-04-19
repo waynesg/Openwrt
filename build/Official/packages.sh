d4a1815c3e6566ac4e3cdd925d28a2b2208e6a87
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-19 23:31:25 +0800

    Update packages.sh

--- build/Official/packages.sh
+++ build/Official/packages.sh
@@ -4,64 +4,64 @@
 [[ ! -d package/waynesg ]] && mkdir -p package/waynesg
 
 # AutoCore
-cp -rf ../immortalwrt/package/emortal/autocore package/waynesg/
-cp -rf ../immortalwrt/package/utils/mhz package/utils/
-cp -rf ../immortalwrt-luci/modules/luci-base/root/usr/libexec/rpcd/luci feeds/luci/modules/luci-base/root/usr/libexec/rpcd/
+cp -rf ./immortalwrt/package/emortal/autocore package/waynesg/
+cp -rf ./immortalwrt/package/utils/mhz package/utils/
+cp -rf ./immortalwrt-luci/modules/luci-base/root/usr/libexec/rpcd/luci feeds/luci/modules/luci-base/root/usr/libexec/rpcd/
 # grant getCPUUsage access
 sed -i 's|"getTempInfo"|"getTempInfo", "getCPUBench", "getCPUUsage"|g' package/waynesg/autocore/files/generic/luci-mod-status-autocore.json
 
 # automount
-cp -rf ../lede/package/lean/automount package/waynesg/
-cp -rf ../lede/package/lean/ntfs3-mount package/waynesg/
+cp -rf ./lede/package/lean/automount package/waynesg/
+cp -rf ./lede/package/lean/ntfs3-mount package/waynesg/
 # backport ntfs3 patches
-cp -rf ../lede/target/linux/generic/files-5.10 target/linux/generic/
+cp -rf ./lede/target/linux/generic/files-5.10 target/linux/generic/
 
 # FullCone nat for nftables
 # patch kernel
-cp -f ../immortalwrt/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
+cp -f ./immortalwrt/target/linux/generic/hack-5.10/952-net-conntrack-events-support-multiple-registrant.patch target/linux/generic/hack-5.10/
 # fullconenat-nft
-cp -rf ../immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
+cp -rf ./immortalwrt/package/network/utils/fullconenat-nft package/network/utils/
 # patch libnftnl
-cp -rf ../immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
+cp -rf ./immortalwrt/package/libs/libnftnl/patches package/libs/libnftnl/
 sed -i '/PKG_INSTALL:=1/i\PKG_FIXUP:=autoreconf' package/libs/libnftnl/Makefile
 # patch nftables
-#cp -f ../immortalwrt/package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch package/network/utils/nftables/patches/
+#cp -f ./immortalwrt/package/network/utils/nftables/patches/002-nftables-add-fullcone-expression-support.patch package/network/utils/nftables/patches/
 rm -rf package/network/utils/nftables/
-cp -rf ../immortalwrt/package/network/utils/nftables package/network/utils/
+cp -rf ./immortalwrt/package/network/utils/nftables package/network/utils/
 # patch firewall4
-cp -rf ../immortalwrt/package/network/config/firewall4/patches package/network/config/firewall4/
+cp -rf ./immortalwrt/package/network/config/firewall4/patches package/network/config/firewall4/
 sed -i 's|+kmod-nft-nat +kmod-nft-nat6|+kmod-nft-nat +kmod-nft-nat6 +kmod-nft-fullcone|g' package/network/config/firewall4/Makefile
 # patch luci
 patch -d feeds/luci -p1 -i ../../../patches/fullconenat-luci.patch
 
 # dnsmasq: add filter aaa option
-cp -rf ../patches/910-add-filter-aaaa-option-support.patch package/network/services/dnsmasq/patches/
-patch -p1 -i ../patches/dnsmasq-add-filter-aaaa-option.patch
+cp -rf ./patches/910-add-filter-aaaa-option-support.patch package/network/services/dnsmasq/patches/
+patch -p1 -i ./patches/dnsmasq-add-filter-aaaa-option.patch
 patch -d feeds/luci -p1 -i ../../../patches/filter-aaaa-luci.patch
 
 # dnsmasq: use nft ruleset for dns_redirect
-patch -p1 -i ../patches/dnsmasq-use-nft-ruleset-for-dns_redirect.patch
+patch -p1 -i ./patches/dnsmasq-use-nft-ruleset-for-dns_redirect.patch
 
 # mbedtls
 rm -rf package/libs/mbedtls
-cp -rf ../immortalwrt/package/libs/mbedtls package/libs/
+cp -rf ./immortalwrt/package/libs/mbedtls package/libs/
 
 ##-------------------------------------------------------------------------------------------------------
 # cpufreq
-cp -rf ../immortalwrt-luci/applications/luci-app-cpufreq package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-cpufreq package/waynesg/
 
 # DDNS
-cp -rf ../immortalwrt-packages/net/ddns-scripts_{aliyun,dnspod} package/waynesg/
+cp -rf ./immortalwrt-packages/net/ddns-scripts_{aliyun,dnspod} package/waynesg/
 
 # Access Control
-cp -rf ../immortalwrt-luci/applications/luci-app-accesscontrol package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-accesscontrol package/waynesg/
 
 # arpbind
-cp -rf ../immortalwrt-luci/applications/luci-app-arpbind package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-arpbind package/waynesg/
 
 # Filetransfer
-cp -rf ../immortalwrt-luci/applications/luci-app-filetransfer package/waynesg/
-cp -rf ../immortalwrt-luci/libs/luci-lib-fs package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-filetransfer package/waynesg/
+cp -rf ./immortalwrt-luci/libs/luci-lib-fs package/waynesg/
 
 # OLED
 svn export -q https://github.com/NateLol/luci-app-oled/trunk package/waynesg/luci-app-oled
@@ -70,13 +70,13 @@ svn export -q https://github.com/NateLol/luci-app-oled/trunk package/waynesg/luc
 svn export -q https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/waynesg/luci-app-openclash
 
 # Realtek R8125, RTL8152/8153, RTL8192EU
-cp -rf ../immortalwrt/package/kernel/{r8125,r8152,rtl8192eu} package/waynesg/
+cp -rf ./immortalwrt/package/kernel/{r8125,r8152,rtl8192eu} package/waynesg/
 
 # Release Ram
-cp -rf ../immortalwrt-luci/applications/luci-app-ramfree package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-ramfree package/waynesg/
 
 # Scheduled Reboot
-cp -rf ../immortalwrt-luci/applications/luci-app-autoreboot package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-autoreboot package/waynesg/
 
 # SeverChan
 svn export -q https://github.com/tty228/luci-app-serverchan/trunk package/waynesg/luci-app-serverchan
@@ -87,23 +87,23 @@ svn export -q https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-lib
 rm -rf ./feeds/packages/net/{xray-core,shadowsocks-libev}
 
 # USB Printer
-cp -rf ../immortalwrt-luci/applications/luci-app-usb-printer package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-usb-printer package/waynesg/
 
 # vlmcsd
-cp -rf ../immortalwrt-luci/applications/luci-app-vlmcsd package/waynesg/
-cp -rf ../immortalwrt-packages/net/vlmcsd package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-vlmcsd package/waynesg/
+cp -rf ./immortalwrt-packages/net/vlmcsd package/waynesg/
 
 # xlnetacc
-cp -rf ../immortalwrt-luci/applications/luci-app-xlnetacc package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-xlnetacc package/waynesg/
 
 # Zerotier
-cp -rf ../immortalwrt-luci/applications/luci-app-zerotier package/waynesg/
+cp -rf ./immortalwrt-luci/applications/luci-app-zerotier package/waynesg/
 
 # default settings and translation
-#cp -rf ../default-settings package/waynesg/
+#cp -rf ./default-settings package/waynesg/
 
 # fix include luci.mk
-find package/waynesg/ -type f -name Makefile -exec sed -i 's,../../luci.mk,$(TOPDIR)/feeds/luci/luci.mk,g' {} +
+find package/waynesg/ -type f -name Makefile -exec sed -i 's,././luci.mk,$(TOPDIR)/feeds/luci/luci.mk,g' {} +
 
 #Alist
 git clone -b master --depth 1 https://github.com/sbwml/luci-app-alist.git package/waynesg/luci-app-alist
@@ -164,7 +164,7 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/waynes
 
 #socat
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/waynesg/luci-app-socat
-cp -rf ../immortalwrt-packages/net/socat package/waynesg/
+cp -rf ./immortalwrt-packages/net/socat package/waynesg/
 
 #passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall -b luci-smartdns-new-version package/waynesg/openwrt-passwall
