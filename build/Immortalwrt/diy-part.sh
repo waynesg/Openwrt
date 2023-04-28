7d35c1b7a75f638470a16ec74e898f8706e9c3b9
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-04-28 09:38:40 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -125,6 +125,7 @@ feeds.buildinfo
 openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
+openwrt-x86-64-generic-squashfs-combined-efi.img.gz
 sha256sums
 version.buildinfo
 EOF
