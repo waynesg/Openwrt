1eec21555be1dbf0ee62f2feef2c60ec2190d974
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-05-01 21:28:35 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -72,6 +72,9 @@ export Delete_unnecessary_items="0"          # 个别机型内一堆其他机型
 export Disable_53_redirection="0"            # 删除DNS强制重定向53端口防火墙规则(个别源码本身不带次功能)(1为启用命令,填0为不作修改)
 export Cancel_running="0"                    # 取消路由器每天跑分任务(个别源码本身不带次功能)(1为启用命令,填0为不作修改)
 
+#关闭串口跑码
+sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
+
 
 #菜单调整
 sed -i 's/\"services\"/\"control\"/g' ./feeds/luci/applications/luci-app-accesscontrol/luasrc/controller/mia.lua
