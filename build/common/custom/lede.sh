df932fae864f1e937efcc47b8d7c807fe606cc20
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-05-05 22:04:02 +0800

    Create lede.sh

---
+++ build/common/custom/lede.sh
@@ -0,0 +1,23 @@
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/waynesg/luci-app-advanced
+git clone -b master --depth 1 https://github.com/sbwml/luci-app-alist.git package/waynesg/luci-app-alist
+svn co https://github.com/sirpdboy/luci-app-autotimeset/trunk package/waynesg/luci-app-autotimeset
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/waynesg/luci-app-bypass
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-cloudflarespeedtest package/waynesg/luci-app-cloudflarespeedtest
+svn co https://github.com/immortalwrt-collections/openwrt-cdnspeedtest/trunk/cdnspeedtest package/waynesg/luci-app-cloudflarespeedtest/cdnspeedtest
+svn co https://github.com/sirpdboy/luci-app-parentcontrol/trunk package/waynesg/luci-app-parentcontrol
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-speedlimit package/waynesg/luci-app-control-speedlimit
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-timewol package/waynesg/luci-app-control-timewol
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-webrestriction package/waynesg/luci-app-control-webrestriction
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-fileassistant package/waynesg/luci-app-fileassistant
+svn co https://github.com/ximiTech/luci-app-msd_lite/trunk package/waynesg/luci-app-msd_lite
+svn co https://github.com/pymumu/luci-app-smartdns/branches/lede package/waynesg/luci-app-smartdns
+svn co https://github.com/sirpdboy/netspeedtest/trunk package/waynesg/netspeedtest
+svn co https://github.com/destan19/OpenAppFilter/trunk package/waynesg/luci-app-oaf
+svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-onliner package/waynesg/luci-app-onliner
+git clone --depth 1 -b master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/waynesg/luci-app-unblockneteasemusic
+svn co https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-airconnect package/waynesg/luci-app-airconnect
+svn co https://github.com/QiuSimons/OpenWrt-Add/trunk/airconnect package/waynesg/luci-app-airconnect/airconnect
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall -b luci-smartdns-new-version package/waynesg/openwrt-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 package/waynesg/openwrt-passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git -b packages package/waynesg/passwall
+git clone --depth 1 https://github.com/fw876/helloworld package/waynesg/helloworld
