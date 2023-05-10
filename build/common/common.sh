b5a1059d7c1ce85fd9fbd100e59c8f72aa121c51
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2023-05-10 08:59:26 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -243,7 +243,7 @@ if [ "${REPO_BRANCH}" == "master" ]; then
 sudo -E apt-get -qq install build-essential clang flex g++ gawk gcc-multilib gettext \
 git libncurses5-dev libssl-dev python3-distutils rsync unzip zlib1g-dev swig libpython3-dev aria2 jq subversion qemu-utils ccache rename libelf-dev libfuse-dev
 sudo -E apt-get -qq purge azure-cli ghc* zulu* hhvm llvm* firefox powershell openjdk* dotnet* google* mysql* php* android*
-sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
+sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /usr/lib/jvm /opt/ghc
 sudo -E apt-get -qq autoremove --purge
 sudo -E apt-get -qq clean ) &
 echo "================================================="
